a34570c1cecea4589f3e228cd4e463fa4fb8370c
Oren Fix <orenfix@gmail.com>
2021-09-12 19:17:05 -0400

    MELD 3.0 updates

--- meld/meld.js
+++ meld/meld.js
@@ -16,31 +16,32 @@ $(document).ready(function() {
 	});
 
 	function calculateMeld() {
-		var inrMeld = $("#inrMeld").val();
-		var biliMeld = $("#biliMeld").val();
+		var inr = $("#inr").val();
+		var bili = $("#bili").val();
 		var creatinine = $("#creatinine").val();
 		var sodium = $("#sodium").val();
+		var albumin = $("#albumin").val();
 		var dialysis = $("#dialysis").prop("checked");
-		var equation = $("#equation").prop("checked");
+		var sex = $("#sex").prop("checked");
 
-		if (dialysis) { creatinine=4 };
-
-		var meld = ((0.957*Math.log(creatinine) + 0.378*Math.log(biliMeld) + 1.12*Math.log(inrMeld) + 0.643).toFixed(1))*10;
-		if (meld>40) { meld=40 };
-
-/*
-		var meldna;
-		if (equation) {  //SRTR or UNOS
-			if (sodium>137) { sodium=137 };
-			if (meld<=11) {
-				meldna = meld;
-				} else {
-				meldna = (meld + 1.32*(137-sodium) - (.033*meld*(137-sodium))).toFixed(0);
+		if (dialysis) { 
+			creatinine=4;
+			var creatinine3=3;
 			}
-		} else { //NEJM
-			meldna = (meld - sodium - (.025*meld*(140-sodium)) + 140).toFixed(0);
+
+		if (creatinine>3) { 
+			creatinine3=3
+		} else {
+			creatinine3=creatinine;
 		}
-*/
+
+		if (sex) { 
+			var ifFemale=1;
+			} else { 
+				ifFemale=0;
+			}
+
+		var meld = ((0.957*Math.log(creatinine) + 0.378*Math.log(bili) + 1.12*Math.log(inr) + 0.643).toFixed(1))*10;
 
 		var meldna;
 		if (sodium>137) { sodium=137 };
@@ -49,7 +50,13 @@ $(document).ready(function() {
 		} else {
 			meldna = (meld + 1.32*(137-sodium) - (.033*meld*(137-sodium))).toFixed(0);
 		}
-		
+
+
+		var meld3 = (1.33*ifFemale + 4.56*Math.log(bili) + 0.82*(137-sodium) - 0.24*(137-sodium)*Math.log(bili) + 9.09*Math.log(inr) + 11.14*Math.log(creatinine3) + 1.85*(3.5-albumin) - 1.83*(3.5-albumin)*Math.log(creatinine3) + 6).toFixed(0);
+
+		var Meld3woAlb = (1.33*ifFemale + 4.56*Math.log(bili) + 0.82*(137-sodium) - 0.24*(137-sodium)*Math.log(bili) + 9.09*Math.log(inr) + 11.14*Math.log(creatinine3) + 6).toFixed(0);
+
+/*
 		var probability;
 		if (meldna>31) {
 			probability = 81;
@@ -68,10 +75,12 @@ $(document).ready(function() {
 			} else {
 				probability = 1;
 			}
-
+*/
+		$("#resultMeld3").html(meld3);
+		$("#resultMeld3woAlb").html(Meld3woAlb);
 		$("#resultMeld").html(meld);
 		$("#resultMeldNa").html(meldna);
-		$("#probability").html(probability+"&#37;");
+/*		$("#probability").html(probability+"&#37;"); */
 	};
 
 	calculateMeld();
@@ -84,7 +93,7 @@ $(document).ready(function() {
 		calculateMeld();
 	});
 
-	$("#equation").change(function() {
+	$("#sex").change(function() {
 		calculateMeld();
 	});
 
@@ -94,58 +103,58 @@ $(document).ready(function() {
 	
 	$("#inrButtonPlus").bind("touchend",function(e) {
 		e.preventDefault();
-		var inrMeld = parseFloat($("#inrMeld").val());
-		inrMeld=(inrMeld+.1).toFixed(1);
-		$("#inrMeld").val(inrMeld).trigger("blur");
+		var inr = parseFloat($("#inr").val());
+		inr=(inr+.1).toFixed(1);
+		$("#inr").val(inr).trigger("blur");
 	});
 
 	$("#inrButtonPlus").click(function(e) {
 		e.preventDefault();
-		var inrMeld = parseFloat($("#inrMeld").val());
-		inrMeld=(inrMeld+.1).toFixed(1);
-		$("#inrMeld").val(inrMeld).trigger("blur");
+		var inr = parseFloat($("#inr").val());
+		inr=(inr+.1).toFixed(1);
+		$("#inr").val(inr).trigger("blur");
 	});	
 
 	$("#inrButtonMinus").bind("touchend",function(e) {
 		e.preventDefault();
-		var inrMeld = parseFloat($("#inrMeld").val());
-		inrMeld=(inrMeld-.1).toFixed(1);
-		$("#inrMeld").val(inrMeld).trigger("blur");
+		var inr = parseFloat($("#inr").val());
+		inr=(inr-.1).toFixed(1);
+		$("#inr").val(inr).trigger("blur");
 	});
 
 	$("#inrButtonMinus").click(function(e) {
 		e.preventDefault();
-		var inrMeld = parseFloat($("#inrMeld").val());
-		inrMeld=(inrMeld-.1).toFixed(1);
-		$("#inrMeld").val(inrMeld).trigger("blur");
+		var inr = parseFloat($("#inr").val());
+		inr=(inr-.1).toFixed(1);
+		$("#inr").val(inr).trigger("blur");
 	});
 
 	$("#biliButtonPlus").bind("touchend",function(e) {
 		e.preventDefault();
-		var biliMeld = parseFloat($("#biliMeld").val());
-		biliMeld=(biliMeld+.1).toFixed(1);
-		$("#biliMeld").val(biliMeld).trigger("blur");
+		var bili = parseFloat($("#bili").val());
+		bili=(bili+.1).toFixed(1);
+		$("#bili").val(bili).trigger("blur");
 	});
 
 	$("#biliButtonPlus").click(function(e) {
 		e.preventDefault();
-		var biliMeld = parseFloat($("#biliMeld").val());
-		biliMeld=(biliMeld+.1).toFixed(1);
-		$("#biliMeld").val(biliMeld).trigger("blur");
+		var bili = parseFloat($("#bili").val());
+		bili=(bili+.1).toFixed(1);
+		$("#bili").val(bili).trigger("blur");
 	});
 
 	$("#biliButtonMinus").bind("touchend",function(e) {
 		e.preventDefault();
-		var biliMeld = parseFloat($("#biliMeld").val());
-		biliMeld=(biliMeld-.1).toFixed(1);
-		$("#biliMeld").val(biliMeld).trigger("blur");
+		var bili = parseFloat($("#bili").val());
+		bili=(bili-.1).toFixed(1);
+		$("#bili").val(bili).trigger("blur");
 	});
 
 	$("#biliButtonMinus").click(function(e) {
 		e.preventDefault();
-		var biliMeld = parseFloat($("#biliMeld").val());
-		biliMeld=(biliMeld-.1).toFixed(1);
-		$("#biliMeld").val(biliMeld).trigger("blur");
+		var bili = parseFloat($("#bili").val());
+		bili=(bili-.1).toFixed(1);
+		$("#bili").val(bili).trigger("blur");
 	});
 
 	$("#creatinineButtonPlus").bind("touchend",function(e) {
@@ -202,6 +211,34 @@ $(document).ready(function() {
 		var sodium = parseFloat($("#sodium").val());
 		sodium-=1;
 		$("#sodium").val(sodium).trigger("blur");
+	});
+
+	$("#albuminButtonPlus").bind("touchend",function(e) {
+		e.preventDefault();
+		var albumin = parseFloat($("#albumin").val());
+		albumin+=.1;
+		$("#albumin").val(albumin).trigger("blur");
+	});
+
+	$("#albuminButtonPlus").click(function(e) {
+		e.preventDefault();
+		var albumin = parseFloat($("#albumin").val());
+		albumin+=.1;
+		$("#albumin").val(albumin).trigger("blur");
+	});
+
+	$("#albuminButtonMinus").bind("touchend",function(e) {
+		e.preventDefault();
+		var albumin = parseFloat($("#albumin").val());
+		albumin-=.1;
+		$("#albumin").val(albumin).trigger("blur");
+	});
+
+	$("#albuminButtonMinus").click(function(e) {
+		e.preventDefault();
+		var albumin = parseFloat($("#albumin").val());
+		albumin-=.1;
+		$("#albumin").val(albumin).trigger("blur");
 	});
 
 });
